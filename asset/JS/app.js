b73765d68c36161c4081152871e552202cd113fb
nath-pvr <nathalie.pouvrasseau.t@gmail.com>
2020-10-27 11:31:09 +0100

    deplacements OK and restricted at border map

--- asset/JS/app.js
+++ asset/JS/app.js
@@ -3,49 +3,64 @@ console.log(map_bg);
 let player = document.querySelector('#player');
 console.log(player);
 
-let x;
-let y;
+let xPosition;
+let yPositon;
+
+
 
 // declaration of 4 deplacement functions
 
 function left() {
+    let hLimit = getLimit();
     /* je récupère la position de l'objet player*/
     x = getComputedStyle(player).left;
     /* je transpose x en integer */
     x = parseInt(x);
+
+    if (x> hLimit.start){
     x = x - 25;
     /* je déclare que la valeur de x = integer + px */
-    x = x + "px";
+    xPosition = x + "px";
     /* j'applique le style left à l'objet player */
-    player.style.left = x;
-    console.log(x)
+    player.style.left = xPosition;
+    }
 }
 
 function right() {
+    let hLimit = getLimit();
+    console.log(hLimit);
     x = getComputedStyle(player).left;
     x = parseInt(x);
+
+    if (x < hLimit.end){
     x = x + 25;
-    x = x + "px";
-    player.style.left = x;
-    console.log(x)
+    xPosition = x + "px";
+    player.style.left = xPosition;
+    }    
 }
 
 function up() {
+    let vLimit = getLimit("V"); 
     y = getComputedStyle(player).top;
     y = parseInt(y);
+
+    if (y > vLimit.start){
     y = y - 25;
-    y = y + "px";
-    player.style.top = y;
-    console.log(y)
+    yPositon = y + "px";
+    player.style.top = yPositon;
+    }  
 }
 
 function down() {
+    let vLimit = getLimit("V");
     y = getComputedStyle(player).top;
     y = parseInt(y);
+
+    if (y < vLimit.end){
     y = y + 25;
-    y = y + "px";
-    player.style.top = y;
-    console.log(x)
+    yPositon = y + "px";
+    player.style.top = yPositon;
+    }
 }
 
 // programation of arrow directed
@@ -53,28 +68,50 @@ function down() {
 window.addEventListener("keydown", function (event) {
     let keycode = event.key;
 
-    switch (keycode){
+    switch (keycode) {
 
         case "ArrowUp":
         case "z":
-            up();
-        break;
+            
+            up();         
+            break;
 
         case "ArrowDown":
         case "s":
             down();
-        break;
-                
+            getLimit("V");
+            break;
+
         case "ArrowLeft":
         case "q":
             left();
-        break;
-               
+            break;
+
         case "ArrowRight":
         case "d":
             right();
-        break;
-                
+            break;
+
         case " ":
     }
 });
+
+
+function getLimit(sType) {
+    let limit = {
+        start: null,
+        end: null,
+    };
+
+    if (map_bg != null) {
+
+        if (sType != undefined && sType == "V") {
+            limit.start = 0;
+            limit.end = 605 - player.offsetHeight;
+        } else {
+            limit.start = 0;
+            limit.end = 605 - player.offsetWidth;
+        }
+    }
+    return limit;
+}
